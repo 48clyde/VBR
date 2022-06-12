d03240eabdf7fbae26b180307bc9b7a5923a3725
Rick Cobb <cobbr2@yahoo.com>
2022-06-12 01:48:48 +0000

    correct uid/gid for ripped files. Get unknown albums to never collide

--- yar/yar.sh
+++ yar/yar.sh
@@ -41,6 +41,32 @@ while true; do
         #
         #
         /usr/bin/abcde -N -d ${CDROM} -o flac -c /etc/abcde-local.conf
+
+	# If it couldn't find the album in CDDB (or whatever's configured
+	# in abcde-local.conf), it'll create an Unknown_Artist/Unkown_Album directory.
+	# Then the *next one* like that will overwrite files in there.
+	# So move it to someplace unique.  The pattern match here is intended
+	# to support both my convention (directory per artist) and the default.
+	#
+	# It's handy that we haven't been able to eject yet :)
+	# Taken from https://unix.stackexchange.com/a/287851
+	#
+	# From a workflow standpoint, it might be better to just use datetime;
+	# I stack my CDs in the order I rip, so being able to sort by time
+	# might work better when tagging. But since LMS/New Music sorts that
+	# way anyway, let's go with a clean key. Since we've inherited
+	# underscores munge the id to replace spaces with underscores. Do I
+	# really want underscores?  I didn't have them in vbox.
+	#
+	# Don't have a clean way to make this respond to custom munge...
+	# functions in abcde.conf. Hmm.
+	dest_dir="/out/flac"
+	if [ -d "${dest_dir}/Unknown_Artist/Unknown_Album" ]; then
+	    disc_id=$(cd-discid /dev/sr0 | tr ' ' '_')
+	    cd "${dest_dir}"
+	    mv "Unknown_Artist/Unknown_Album" "Unknown_Artist/Unknown_Album_$disc_id"
+	    cd "${OLDPWD}"
+	fi
         
         #
         # Tell LMS to rescan the library to pick-up these new files
